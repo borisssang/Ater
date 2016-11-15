d5358fb8b9c7686455ec526a335163d1c4c4fe00
borisssang <borisssang@gmail.com>
2016-11-15 02:38:58 +0200

    summary
    :D

---
+++ MenuItem.swift
@@ -0,0 +1,22 @@
+//
+//  MenuItem.swift
+//  Ater
+//
+//  Created by Boris Angelov on 11/14/16.
+//  Copyright © 2016 Boris Angelov. All rights reserved.
+//
+
+import Foundation
+
+public class MenuItem: NSObject {
+    
+   public var name: String
+    public var ingridients: String
+   // public var description: String
+   
+    override init(){
+      //  description = "This is some random description"
+        name = "PARJOLA"
+        ingridients = "marina, marina, marina"
+    }
+}
---
+++ MenuTableViewController.swift
@@ -0,0 +1,96 @@
+//
+//  MenuTableViewController.swift
+//  Ater
+//
+//  Created by Boris Angelov on 11/14/16.
+//  Copyright © 2016 Boris Angelov. All rights reserved.
+//
+
+import UIKit
+
+class MenuTableViewController: UITableViewController {
+    var numberOfMenuItems: Int = 12
+
+    
+    var menuItems = Array<MenuItem>(){
+        didSet{
+            tableView.reloadData()
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+self.clearsSelectionOnViewWillAppear = false
+
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+               return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return menuItems.count
+    }
+
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "My Cell", for: indexPath)
+
+        let menuItem = menuItems[indexPath.row]
+        
+        if let menuItemCell = cell as? MenuItemTableViewCell {
+            menuItemCell.menuItem = menuItem
+        }
+       return cell
+    }
+
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Delete the row from the data source
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
