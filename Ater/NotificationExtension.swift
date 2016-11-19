0909ba6711e635b528d9ca5957c0e969c94ebe84
NikolaBozhkov <n.bozhkov.ads@gmail.com>
2016-11-19 16:57:53 +0000

    added extension for notifications

---
+++ Ater/NotificationExtension.swift
@@ -0,0 +1,13 @@
+//
+//  NotificationExtension.swift
+//  Ater
+//
+//  Created by Nikola Bozhkov on 11/19/16.
+//  Copyright © 2016 Nikola Bozhkov. All rights reserved.
+//
+
+import Foundation
+
+extension Notification.Name {
+    public static let onCategoriesLoaded = Notification.Name("onCategoriesLoaded")
+}
